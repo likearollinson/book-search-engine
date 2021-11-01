3cca40b14f136dd12e946607e054e10c6df5ac8d
Brad R <brad.w.rollinson@gmail.com>
2021-11-01 19:20:52 -0400

    remove requirement for description from book model

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -3,20 +3,20 @@ const { gql } = require('apollo-server-express');
 const typeDefs = gql`
   type Book {
     _id: ID
-    authors: String
+    authors: [String]
     description: String
-    bookId: String
+    bookId: String!
     image: String
     link: String
-    title: String
+    title: String!
   }
 
   type User {
     _id: ID
-    username: String
-    email: String
-    password: String
-    savedBooks: [Book]!
+    username: String!
+    email: String!
+    password: String!
+    savedBooks: [Book]
   }
 
   type Auth {
@@ -24,17 +24,24 @@ const typeDefs = gql`
     user : User
   }
 
+  input SavedBookInput {
+    authors: [String]
+    title: String
+    description: String
+    bookId: String
+    image: String
+    link: String
+  }
+
   type Query {
-    users: [User]!
-    user(userId: ID!): User
     me: User
   }
 
   type Mutation {
     createUser(username: String!, email: String!, password: String!) : Auth
     login(email: String!, password: String!) : Auth
-    saveBook(userId: ID!, savedBook: String!): User
-    deleteBook(savedBook: String!): User
+    saveBook(input: SavedBookInput): User
+    deleteBook(bookId: String!): User
   }
 `;
 
